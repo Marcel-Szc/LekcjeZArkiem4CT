54161d8228283c3ef96ab251333f81937fbcaa1f
Marcel-Szc <gnpmapper@gmail.com>
2025-10-19 15:31:39 +0200

    19.10.2025ZadDom

---
+++ ProjektowanieOpr/ZadDom19.10.2025/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,34 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Szyfr szyfr = new();
+        Console.WriteLine(szyfr.Cezar());
+    }
+}
+class Szyfr
+{
+    public string Cezar()
+    {
+        string text = "Ala ma kotaz";
+        int shift = 3;
+        string wynik = "";
+
+        for(int i = 0; i < text.Length; i++)
+        {
+            char c = text[i];
+            if (char.IsLetter(c))
+            {
+                char d = char.IsUpper(c) ? 'A' : 'a';
+                char shifted = (char)((((c + shift) - d) % 26) + d);
+                wynik += shifted;
+            }
+            else
+            {
+                wynik += c;
+            }
+        }
+
+        return wynik;
+    }
+}
