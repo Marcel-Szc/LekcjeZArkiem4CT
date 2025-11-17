a35503e425d42e7742864bca43863efae6f593ed
HasanPikerShockColar <mail@mail.com>
2025-11-17 15:37:29 +0100

    17.11.2025v2

--- ProjektowanieOpr/17.11.2025/Zadanie2/src/App.jsx
+++ ProjektowanieOpr/17.11.2025/Zadanie2/src/App.jsx
@@ -1,10 +1,25 @@
-import { useEffect, useState } from 'react'
+import { useEffect,useState } from 'react'
+import Osoba1 from "./components/Osoba1"
+import Osoba2 from "./components/Osoba2"
+import Wybierz from "./components/Wybierz"
 
 export default function App() {
+  const [sterowanie,setSterowanie] = useState(0);
+  const [operacje,setOperacje] = useState(<Wybierz/>);
 
+  useEffect(()=>{
+    if(sterowanie===0)  setOperacje(<Wybierz/>)
+    if(sterowanie==="1")  setOperacje(<Osoba1/>)
+    if(sterowanie==="2")  setOperacje(<Osoba2/>)
+  },[sterowanie]);
   return (
     <>
-      
+      <select onChange={(e)=>setSterowanie(e.target.value)}>
+        <option value="1">Goerge Droyd</option>
+        <option value="2">Charlie Kirk</option>
+      </select>
+      <hr />
+      {operacje}
     </>
   )
 }
---
+++ ProjektowanieOpr/17.11.2025/Zadanie2/src/components/Osoba2.jsx
@@ -0,0 +1,13 @@
+export default function Osoba2()
+{
+    return(
+        <>
+            <h1>Osoba 2:</h1>
+            <div>Imie: Charlie</div>
+            <hr />
+            <div>Nazwisko: Kirk</div>
+            <hr />
+            <img src="https://media1.tenor.com/m/DTetkjrk-oYAAAAC/charlie-kirk-kirkified.gif" alt="George Droyd" width="498px" height="405px"/>
+        </>
+    )
+}
