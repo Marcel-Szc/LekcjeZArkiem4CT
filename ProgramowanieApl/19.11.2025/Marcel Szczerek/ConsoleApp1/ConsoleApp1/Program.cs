006522107c87b03d6a5b54155651938221ce6a21
pzs1 <mail@mail.com>
2025-11-19 08:51:13 +0100

    19.11.2025

---
+++ ProgramowanieApl/19.11.2025/Marcel Szczerek/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Podaj liczbę wierszy: ");
+        int iloscWierszy = Convert.ToInt32(Console.ReadLine());
+        Trojkat trojkat = new();
+        trojkat.Pascal(iloscWierszy);
+        Console.ReadKey();
+    }
+}
+class Trojkat
+{
+    public void Pascal(int wiersze)
+    {
+        int[,] table = new int[wiersze, wiersze];
+        for (int i = 0; i < wiersze; i++)
+        {
+            table[i, 0] = 1;
+            for (int j =1; j <= i; j++)
+            {
+                if(j == i)
+                {
+                    table[i, j] = 1;
+                }
+                else
+                {
+                    table[i, j] = table[i - 1, j - 1] + table[i - 1, j];
+                }
+            }
+        }
+        for (int i = 0; i < wiersze; i++) 
+        {
+            int suma = 0;
+            int LiczbyPierwsze = 0;
+            Console.Write(" ");
+            for(int j = 0; j <= i; j++)
+            {
+                if (table[i, j] % 2 != 0) { LiczbyPierwsze += table[i,j]; }
+                Console.Write(table[i, j] + "  ");
+                suma += table[i, j];
+            }
+            if (i == wiersze - 1)
+            {
+                Console.Write($"\nSuma: {suma} | Suma liczb pierwszych: {LiczbyPierwsze}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
