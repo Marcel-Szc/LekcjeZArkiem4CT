b9e9fe5f14feb62aea15c3dbf3ffc4fc0da1efbd
Detal Mick <costam@mail.com>
2026-02-04 08:33:07 +0100

    04.02.2026

---
+++ ProjektowanieOpr/04.02.2026/projekt/src/app/components/dyrektywy/dyrektywy.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dyrektywy',
+  standalone: false,
+  templateUrl: './dyrektywy.html',
+  styleUrl: './dyrektywy.css',
+})
+export class Dyrektywy {
+  isActive: boolean = false; 
+}
---
+++ ProjektowanieOpr/04.02.2026/zadanie/src/app/components/zdjecia/zdjecia.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-zdjecia',
+  standalone: false,
+  templateUrl: './zdjecia.html',
+  styleUrl: './zdjecia.css',
+})
+export class Zdjecia {
+  isActive1: boolean = false;
+  isActive2: boolean = false;
+  isActive3: boolean = false;
+  isActive4: boolean = false;
+  isActive5: boolean = false;
+}
