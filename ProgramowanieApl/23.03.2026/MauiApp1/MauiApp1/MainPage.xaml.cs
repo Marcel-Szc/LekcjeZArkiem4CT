932bb6b4c7bc4d7e9c8cca90c2245faf25a7953a
Detal Mick <costam@mail.com>
2026-03-23 13:57:41 +0100

    23.03.2036

---
+++ ProgramowanieApl/23.03.2026/MauiApp1/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using Windows.Graphics.Printing;
+
+namespace MauiApp1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            Title = "PZS NR1";
+            List<ListaNazw> lista = new List<ListaNazw>() { 
+                new ListaNazw { Imie="Arkadiusz", Nazwisko="Waliczek" },
+                new ListaNazw { Imie="Charlie", Nazwisko="Kirk" },
+                new ListaNazw { Imie="Benjamin", Nazwisko="Netanyahu" },
+                new ListaNazw { Imie="George", Nazwisko="Floyd" },
+            };
+            MojaLista.ItemsSource = lista;
+        }
+        public class ListaNazw
+        {
+            public string Imie { get; set; }
+            public string Nazwisko { get; set; }
+        }
+
+        private async void ListaWybrana(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem is ListaNazw listaNazw)
+            {
+                await DisplayAlert("Wybrano z Listy: ",$"Nazwa: {listaNazw.Imie} {listaNazw.Nazwisko}","OK");
+            }
+        }
+    }
+}
